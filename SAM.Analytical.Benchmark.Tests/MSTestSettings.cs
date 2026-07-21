89089df87a65a5b1177168783f3b6cf77fcf5032
Michal Dengusiak <tassimulation@gmail.com>
2026-07-21 15:40:53 +0200

    chore: add required SPDX headers

--- SAM.Analytical.Benchmark.Tests/MSTestSettings.cs
+++ SAM.Analytical.Benchmark.Tests/MSTestSettings.cs
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (c) 2020-2026 Michal Dengusiak & Jakub Ziolkowski and contributors
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 [assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
